51a50326f293eb46efe0f74249c584f7dd40cc00
김하늘 <110077343+kimskyyyy@users.noreply.github.com>
2022-12-22 17:58:01 +0900

    Revert "Post merge"

--- src/main/java/sideproject/petmeeting/post/repository/HeartPostRepository.java
+++ src/main/java/sideproject/petmeeting/post/repository/HeartPostRepository.java
@@ -1,7 +1,7 @@
 package sideproject.petmeeting.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 import sideproject.petmeeting.member.domain.Member;
 import sideproject.petmeeting.post.domain.HeartPost;
 import sideproject.petmeeting.post.domain.Post;
@@ -12,12 +12,12 @@ import java.util.Optional;
 public interface HeartPostRepository extends JpaRepository<HeartPost, Long> {
 
     // 좋아요 여부 확인
-    Optional<HeartPost> findByPostAndMember(@Param("post")Post post, @Param("member") Member member);
+    Optional<HeartPost> findByPostAndMember(Post post, Member member);
 
     // Heart Count 구하기
-    Integer countByPostId(@Param("postId")Long PostId);
+    @Query(value = "SELECT COUNT(h.id) FROM HeartPost h WHERE h.post.id = :PostId")
+    int findCountHeart(Long PostId);
 
-    // 마이페이지 '좋아요'한 게시글 조회
     List<HeartPost> findAllByMemberId(Long id);
 
 }
