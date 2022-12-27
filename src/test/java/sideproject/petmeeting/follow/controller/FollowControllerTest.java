fa697a9adec3d1f6ffacfcff0f49c9994af37efb
BeomSeogKim <kbs4520@daum.net>
2022-12-28 01:04:44 +0900

    Test 코드 수정 - ASCII

--- src/test/java/sideproject/petmeeting/follow/controller/FollowControllerTest.java
+++ src/test/java/sideproject/petmeeting/follow/controller/FollowControllerTest.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 import sideproject.petmeeting.follow.domain.Follow;
 import sideproject.petmeeting.follow.domain.dto.FollowRequestDto;
 import sideproject.petmeeting.follow.repository.FollowRepository;
@@ -23,6 +28,12 @@ import sideproject.petmeeting.member.repository.MemberRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +56,8 @@ class FollowControllerTest {
     FollowRepository followRepository;
 
     @BeforeEach
-    void setting() {
+    void setting(WebApplicationContext webApplicationContext,
+                 RestDocumentationContextProvider restDocumentationContextProvider) {
         Member member1 = Member.builder()
                 .nickname("Tommy")
                 .password("test")
@@ -63,6 +75,14 @@ class FollowControllerTest {
                 .build();
         memberRepository.save(member1);
         memberRepository.save(member2);
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentationContextProvider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(modifyUris().host("tommy.me").removePort(), prettyPrint())
+                        .withResponseDefaults(modifyUris().host("tommy.me").removePort(), prettyPrint()))
+                .alwaysDo(print())
+                .build();
     }
     @Test
     void follow() throws Exception {
@@ -74,7 +94,27 @@ class FollowControllerTest {
                 .accept(HAL_JSON)
                 .content(objectMapper.writeValueAsString(followRequestDto)))
                 .andExpect(status().isCreated())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document( "request follow",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("access token"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        ),
+                        requestFields(
+                                fieldWithPath("followMemberId").description("member id to follow")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").description("status of action"),
+                                fieldWithPath("message").description("message of action"),
+                                fieldWithPath("data.object").description("message of action"),
+                                fieldWithPath("data.links[0].rel").description("relation"),
+                                fieldWithPath("data.links[0].href").description("url of action")
+                        )
+                ));
         assertThat(followRepository.findAll().size()).isEqualTo(1);
         assertThat(followRepository.findByFollowing(lisa)).isPresent();
     }
@@ -89,9 +129,24 @@ class FollowControllerTest {
                 .build());
 
         this.mockMvc.perform(delete("/api/follow/" + follow.getId())
-                .header("Authorization", getAccessToken()))
+                        .header("Authorization", getAccessToken()))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("delete follow",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("access token")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").description("status of action"),
+                                fieldWithPath("message").description("message of action"),
+                                fieldWithPath("data.object").description("message of action"),
+                                fieldWithPath("data.links[0].rel").description("relation"),
+                                fieldWithPath("data.links[0].href").description("url of action")
+                        )
+                ));
 
         assertThat(followRepository.findAll().size()).isEqualTo(0);
     }
@@ -106,9 +161,25 @@ class FollowControllerTest {
                 .build());
 
         this.mockMvc.perform(get("/api/follow/follow-member")
-                .header("Authorization", getAccessToken()))
+                        .header("Authorization", getAccessToken()))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("get follow member list",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("access token")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").description("status of action"),
+                                fieldWithPath("message").description("message of action"),
+                                fieldWithPath("data.object[0].memberId").description("id of follow member"),
+                                fieldWithPath("data.object[0].memberNickname").description("nickname of follow member"),
+                                fieldWithPath("data.object[0].memberImage").description("image of follow member"),
+                                fieldWithPath("data.links[0].rel").description("relation"),
+                                fieldWithPath("data.links[0].href").description("url of action")
+                        )));
     }
     @Test
     void getFollower() throws Exception {
@@ -122,7 +193,23 @@ class FollowControllerTest {
         this.mockMvc.perform(get("/api/follow/follower")
                         .header("Authorization", getAccessToken()))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("get follower list",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("access token")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").description("status of action"),
+                                fieldWithPath("message").description("message of action"),
+                                fieldWithPath("data.object[0].memberId").description("id of follower"),
+                                fieldWithPath("data.object[0].memberNickname").description("nickname of follower"),
+                                fieldWithPath("data.object[0].memberImage").description("image of follower"),
+                                fieldWithPath("data.links[0].rel").description("relation"),
+                                fieldWithPath("data.links[0].href").description("url of action")
+                        )));
     }
 
     @Test
