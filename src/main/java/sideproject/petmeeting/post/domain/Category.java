0b6005fb1629ba526760844ab41a7913ad77370f
kimskyyyy <hhhhh823@naver.com>
2023-01-12 18:58:34 +0900

    게시글 검색, 카테고리별 조회 추가 #6

--- src/main/java/sideproject/petmeeting/post/domain/Category.java
+++ src/main/java/sideproject/petmeeting/post/domain/Category.java
@@ -6,11 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Category {
-    SHARE("공유"),
-    HELP("도움"),
-    RECOMMEND("추천"),
+    SHARE,
+    HELP,
+    RECOMMEND,
+    TALK
 
-    TALK("잡담");
-
-    final String value;
 }
